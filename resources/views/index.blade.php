021a39ab509404055ad443def20d51845bc7da3a
franciscampos91 <francis.goes@yahoo.com.br>
2024-05-31 00:14:25 -0300

    commit template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,19 +3,61 @@
 
 @section('content')
 
-<div>
-    <h1>Cursos</h1>
+<div class="container">
+
+    <div class="menu__icon">
+
+        <div class="box__icon">
+            <a href="/cursos">    
+                <div class="icon">
+                    <i class="bi bi-globe2"></i>
+                </div>
+                <div class="label">
+                    Cursos
+                </div>
+            </a>
+        </div>
+    
+        <div class="box__icon">
+            <a href="/alunos">    
+                <div class="icon">
+                    <i class="bi bi-person"></i>
+                </div>
+                <div class="label">
+                    Alunos
+                </div>
+            </a>
+        </div>
+    
+        <div class="box__icon">
+            <a href="/professores">    
+                <div class="icon">
+                    <i class="bi bi-person-video3"></i>
+                </div>
+                <div class="label">
+                    Professores
+                </div>
+            </a>
+        </div>
+        
+        <div class="box__icon">
+            <a href="/unidades">    
+                <div class="icon">
+                    <i class="bi bi-bank"></i>
+                </div>
+                <div class="label">
+                    Unidades
+                </div>
+            </a>
+        </div>
+
+    </div>
+
+
+
+
+
 </div>
-
-<ul>
-    <a href="/disciplinas"><li>Disciplinas</li></a>
-    <a href="/professores"><li>Professores</li></a>
-    <a href="/alunos"><li>Alunos</li></a>
-</ul>
-
-@foreach ($cursos as curso)
-    <h1></h1>
-@endforeach
 
 
 @endsection
